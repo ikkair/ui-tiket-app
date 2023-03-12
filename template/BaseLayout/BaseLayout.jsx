1beb05f5e320c6d4ca03e24d1a7b7cc53ef8e6f2
Shaniara28 <saniarizkiagustin@gmail.com>
2023-03-12 11:34:53 +0700

    add footer and landing page

--- template/BaseLayout/BaseLayout.jsx
+++ template/BaseLayout/BaseLayout.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Navbar } from "../../components/Navbar/Navbar";
-import style from "./BaseLayout.module.css";
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import { Navbar } from '../../components/Navbar/Navbar';
+import style from './BaseLayout.module.css';
 
 const BaseLayout = ({ children }) => {
   return (
     <>
       <Navbar />
       <main className={`${style.mainContent}`}>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
