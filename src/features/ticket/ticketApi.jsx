0302c9fe53482e8ffc77110d24be6ae8b4402298
mhmdiamd <darmawanilham34@gmail.com>
2023-03-15 01:11:45 +0700

    add Redux get ticket

---
+++ src/features/ticket/ticketApi.jsx
@@ -0,0 +1,21 @@
+import { apiSlice } from "../../app/api/authApi"
+
+export const ticketApi = apiSlice.injectEndpoints({
+  tagTypes: ['getTicketByIdPassenger'],
+  endpoints: (builder) => ({
+ 
+    getTicketByIdPassenger: builder.query({
+      query: (id) =>  ({
+        url: `tickets?id_passenger=${id}`
+      }),
+
+      providesTags: ['getTicketByIdPassenger'],
+      transformResponse: (response, meta, args) => response.data
+    }),
+
+
+   
+  })
+})
+
+export const { useGetTicketByIdPassengerQuery } = ticketApi 
